b5adb8adab9df64b30ee827cbafc384d7ecf549b
Sergio Nieto <snieto10@gmail.com>
2022-10-13 14:06:12 -0400

    adding javascript

--- js/main.js
+++ js/main.js
@@ -18,47 +18,97 @@
 //   () =>
 //     tareaPablo(
 //       "[Pablo] hace miniatura",
-//       () => pesada(" --> Revisar miniatura"),
-//       2000
+//       () => simple(" --> Revisar miniatura"),
+//       10000
 //     ),
 //   () => pesada("Grabar video"),
 //   () =>
-//     tareaPablo("[Pablo] Editar video", () => simple(" --> Revisar video"), 5000),
+//     tareaPablo("[Pablo] Editar video", () => simple(" --> Publicar video"), 10),
 // ];
 
 // for (const tarea of tareas) tarea();
 
-const userLeft = false;
-const userWatchingCatMeme = false;
+/*  OTHER EXERCISE*/
 
-console.log("sergio1");
+// const userLeft = false;
+// const userWatchingCatMeme = false;
 
-function watchTutorialCallback(callback, errorCallback) {
-  if (userLeft) {
-    errorCallback({
-      name: "UserLeft",
-      message: ":(",
-    });
-  } else if (userWatchingCatMeme) {
-    errorCallback({
-      name: "User watching Cat Meme",
-      message: "WebDev < Car",
-    });
+// console.log("sergio1");
+
+// function watchTutorialCallback(callback, errorCallback) {
+//   if (userLeft) {
+//     errorCallback({
+//       name: "UserLeft",
+//       message: ":(",
+//     });
+//   } else if (userWatchingCatMeme) {
+//     errorCallback({
+//       name: "User watching Cat Meme",
+//       message: "WebDev < Car",
+//     });
+//   } else {
+//     callback("Thumbs up and Subscribe");
+//   }
+// }
+// console.log("sergio2");
+
+// watchTutorialCallback(
+//   (message) => {
+//     console.log(message);
+//   },
+//   (error) => {
+//     console.log(error.name + " " + error.message);
+//   }
+// );
+
+// console.log("sergio3");
+
+/*  OTHER EXERCISE*/
+
+// const error = (msg) => console.log(msg);
+
+// const tarea = (tarea, siguiente, error) => {
+//   console.log(tarea);
+//   if (Math.random() < 0.1) error();
+//   setTimeout(siguiente, 500);
+// };
+
+// const pensarIdea = () =>
+//   tarea("pensar idea", guionizar, () => "Error al pensar idea");
+
+// const guionizar = () =>
+//   tarea("guionizar", grabar, () => error("Error al guionizar"));
+
+// const grabar = () => tarea("grabar", editar, () => error("Error al grabar"));
+
+// const editar = () => tarea("Editar", publicar, () => error("Error al editar"));
+
+// const publicar = () =>
+//   tarea(
+//     "Publicar",
+//     () => console.log("Publicado"),
+//     () => error("Error al publicar")
+//   );
+
+// pensarIdea();
+
+const toNumber = (n) => Number(n);
+const multiply = (n) => n * 2;
+const print = (n) => console.log(n);
+
+const req = new XMLHttpRequest();
+req.open("GET", "./numero.txt");
+
+req.onload = () => {
+  if (req.status === 200) {
+    console.log(req.responseText);
   } else {
-    callback(() => {
-      console.log("great");
-    });
+    console.log("Error");
   }
-}
-console.log("sergio2");
+};
 
-watchTutorialCallback(
-  (message) => {
-    console.log(message);
-  },
-  (error) => {
-    console.log(error.name + " " + error.message);
-  }
-);
+req.onerror = () => {
+  console.log("Error");
+};
 
-console.log("sergio3");
+req.send();
